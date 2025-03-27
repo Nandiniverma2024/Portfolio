8603551167ea6ffb8cdd98bbcb2cc1a10247cd18
Nandini <nandiniverma202021@gmail.com>
2025-03-27 12:27:59 +0530

    fix build errors

--- app/components/ui/BentoGrid.tsx
+++ app/components/ui/BentoGrid.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import animationData from "@/data/confetti.json";
 import MagicButton from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
+import Image from "next/image";
 
 export const BentoGrid = ({
   className,
@@ -75,7 +76,7 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img &&(        //If a image exist, we want to render an image 
-            <img
+            <Image
               src={img}
               alt={img}
               className={cn(imgClassName, 'object-cover, object-center')}
@@ -84,7 +85,7 @@ export const BentoGridItem = ({
         </div>
         <div className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"}`}>
           {spareImg && (
-            <img
+            <Image
               src={spareImg}
               alt={spareImg}
               className={'object-cover, object-center, w-full, h-full'}
