ae37ff5b05ed55126a6b8787df2d2a6ab96a9e8a
Nandini <nandiniverma202021@gmail.com>
2025-03-27 14:57:13 +0530

    fix

--- app/components/ui/MovingBorders.tsx
+++ app/components/ui/MovingBorders.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { RefObject } from "react";
+import React from "react";
 import {
   motion,
   useAnimationFrame,
@@ -81,7 +81,7 @@ export const MovingBorder = ({
   duration?: number;
   rx?: string;
   ry?: string;
-  [key: string]: any;
+  [key: string]: unknown;
 }) => {
   const pathRef = useRef<SVGRectElement | null>(null);
   const progress = useMotionValue<number>(0);
